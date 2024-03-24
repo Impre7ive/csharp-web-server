03d8ba232fb9e8c37044904a45cb7b71f5821709
Dmitry Liashenko <dmitry.liash9nko@gmail.com>
2024-03-24 19:10:34 +0200

    Created basic structure for the HTTP handler.

---
+++ WebServer/HttpServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+	internal class HttpServer
+	{
+		private ManualResetEvent signal;
+		private TcpListener listener;
+
+		public HttpServer(IPAddress ip, int port, ManualResetEvent signal) { 
+			listener = new TcpListener(ip, port);
+			this.signal = signal;
+		}
+		public void Listen() {
+			listener.Start();
+
+			do
+			{
+				var client = listener.AcceptTcpClient();
+				Console.WriteLine($"Incoming request from: {client.Client.RemoteEndPoint}");
+				Thread.Sleep(1000);
+			} 
+			while (!signal.WaitOne());
+
+			listener.Stop();
+		}
+	}
+}
--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace WebServer
+﻿using System.Net;
+
+namespace WebServer
 {
 	internal class Program
 	{
+		private static ManualResetEvent stopSignal = new ManualResetEvent(false);
+		private static IPAddress ip = IPAddress.Any;
+		private static int httpPort  = 80;
+
 		static void Main(string[] args)
 		{
-			//Console.WriteLine("Hello, World!");
+			WelcomeMessage();
+
+			var httpServer = new HttpServer(ip, httpPort, stopSignal);
+			var httpHandlerThread = new Thread(new ThreadStart(httpServer.Listen));
+			httpHandlerThread.Start();
+
+			ConsoleKey key;
+
+			do
+			{
+				key = Console.ReadKey(false).Key;
+			} while (key != ConsoleKey.Q);
+
+			stopSignal.Set();
+			httpHandlerThread.Join();
+		}
+
+		private static void WelcomeMessage()
+		{
+			Console.WriteLine("Welcome to C# Web Server.");
+			Console.WriteLine("Press Q to stop server...");
 		}
 	}
 }
